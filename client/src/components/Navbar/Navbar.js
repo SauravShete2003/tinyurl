ca1b167f27060cc7a8937c5c7c96b3f0f2126983
sauravshete2003 <sauravshete72@gmail.com>
2024-08-07 22:48:21 +0530

    add media query

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -31,37 +31,17 @@ function Navbar() {
     <div className="navbar-container">
       <h1 className="navbar-heading">Tiny URL</h1>
       <img src={MenuIcon} className="menu-icon" onClick={toggleMenu} alt="Menu" />
-      <div className={`menu-icon-container ${isMenu ? "active" : ""}`}>
-        <Link to="/" className="menu-navbar-item" onClick={toggleMenu}>
+      <div className={`nav-item-container ${isMenu ? "active" : ""}`}>
+        <Link to="/" className="nav-item" onClick={toggleMenu}>
           Home
         </Link>
-        <Link to="/" className="menu-navbar-item" onClick={toggleMenu}>
+        <Link to="/" className="nav-item" onClick={toggleMenu}>
           Generate
         </Link>
-        <Link to="/alllinks" className="menu-navbar-item" onClick={toggleMenu}>
+        <Link to="/alllinks" className="nav-item" onClick={toggleMenu}>
           My Links
         </Link>
-        <Link to="/login" className="menu-navbar-item" onClick={toggleMenu}>
-          LogIn
-        </Link>
-        {user.name && <span className="logged-user">{user.name}</span>}
-        {user.name && (
-          <span className="logout-btn" onClick={handleLogout}>
-            LogOut
-          </span>
-        )}
-      </div>
-      <div className="nav-item-container">
-        <Link to="/" className="nav-item">
-          Home
-        </Link>
-        <Link to="/" className="nav-item">
-          Generate
-        </Link>
-        <Link to="/alllinks" className="nav-item">
-          My Links
-        </Link>
-        <Link to="/login" className="nav-item">
+        <Link to="/login" className="nav-item" onClick={toggleMenu}>
           LogIn
         </Link>
         {user.name && <span className="nav-item logged-user">{user.name}</span>}
